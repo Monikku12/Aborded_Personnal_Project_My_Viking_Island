7594e8be2ef53ecc62131106be52952a9c6eb91e
Monique Payment-Boulanger <monique.paymentboulanger@gmail.com>
2022-11-23 00:18:38 +0900

    added base files

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Bag from "./src/components/bag";
+import Bedroom from "./src/components/bedroom";
+import Blacksmith from "./src/components/blacksmith";
+import Farm from "./src/components/farm";
+import Great_Hall from "./src/components/great_hall";
+import Necklace_Table from "./src/components/necklace_table";
+import Options from "./src/components/options";
+import QuestList from "./src/components/quest_list";
+import ReferenceBook from "./src/components/reference_book";
+import Title from "./src/components/title";
+import Village from "./src/components/village";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <NativeBaseProvider>
+                <Box>Hello world</Box>
+            </NativeBaseProvider>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
